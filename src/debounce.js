8233a0b25019334761000e2c13a962e3fb73823d
conorhastings <conor@socialtables.com>
2017-11-09 10:24:43 -0500

    add unique prop HOC

--- src/debounce.js
+++ src/debounce.js
@@ -1,9 +1,16 @@
 import React from "react";
 import debounce from "lodash.debounce";
+import uniquePropHOC from "./lib/unique-prop-hoc";
 
-export class Debounce extends React.Component {
-	state = { showChildren: false };
-	onMouseMove = debounce(() => this.setState({ showChildren: true }), this.props.seconds * 1000);
+class Debounce extends React.Component {
+	constructor() {
+		super();
+		this.state = { component: null };
+		this.onMouseMove = debounce(this.onMouseMove(this.props.time || this.props.seconds * 1000));
+	}
+	onMouseMove() {
+		this.setState({ component: this.props.children });
+	}
 	componentDidMount() {
 		document.body.addEventListener("mousemove", this.onMouseMove);
 	}
@@ -11,7 +18,9 @@ export class Debounce extends React.Component {
 		document.body.removeEventListener("mousemove", this.onMouseMove);
 	}
 	render() {
-		return this.state.showChildren ? this.props.chidren : null;
+		return this.state.component;
 	}
 }
+
+export default uniquePropHOC(["time", "seconds"])(Debounce);
 
--- src/expire.js
+++ src/expire.js
@@ -1,24 +1,21 @@
 import React from "react";
-export default class Expire extends React.Component {
+import uniquePropHOC from "./lib/unique-prop-hoc";
+
+class Expire extends React.Component {
     constructor(props) {
         super(props);
         this.state = { component: props.children }
     }
     componentDidMount() {
-        if (this.props.time) {
-            setTimeout(() => {
-                this.setState({
-                    component: null
-                });
-            }, this.props.time);
-        }
-        else {
+        setTimeout(() => {
             this.setState({
                 component: null
             });
-        }
+        }, this.props.time || this.props.seconds * 1000);
     }
     render() {
         return this.state.component;
     }
 }
+
+export default uniquePropHOC(["time", "seconds"])(Expire);
